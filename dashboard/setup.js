29989af7979bc0378632acb092e32294d44dffa0
itsfrosea <frostyyuniverse@gmail.com>
2026-03-07 04:46:14 +0530

    updated

--- dashboard/setup.js
+++ dashboard/setup.js
@@ -1,20 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
 
 const params = new URLSearchParams(window.location.search);
-
 const channel = params.get("channel");
 
 const text = document.getElementById("channelText");
 
 if(channel){
-
 text.innerHTML =
-"✅ Connected Twitch channel: <strong>" + channel + "</strong>";
-
+"Connected channel: <strong>" + channel + "</strong>";
 }else{
-
 text.innerHTML = "Channel connected.";
-
 }
 
 const btn = document.getElementById("openDashboard");
